37c9dc5e3d89906f57e6a4a94402e9857cdfe77e
linsalefe <linsalefe@gmail.com>
2025-06-18 16:27:08 -0300

    Update dashcongresso.py

--- pages/dashcongresso.py
+++ pages/dashcongresso.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-df = pd.read_csv('dashcongresso.csv')
+df = pd.read_csv('../dashcongresso.csv')
 
 st.set_page_config(layout='wide')
 st.title ('Dash Congressos CENAT Junho - 2025')
